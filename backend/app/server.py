3b324c7f379a9a19701d4007a74bca38c37888b3
imp050t0r <106441783+imp050t0r@users.noreply.github.com>
2026-08-25 12:08:52 +0200

    Register seed quantity API

--- backend/app/server.py
+++ backend/app/server.py
@@ -1,5 +1,6 @@
 from app.main import app
 from app.master_data_routes import router as master_data_router
+from app.seed_quantity_routes import router as seed_quantity_router
 from app.seeding_data_routes import router as seeding_data_router
 from app.successor_routes import router as successor_router
 
@@ -7,3 +8,4 @@ from app.successor_routes import router as successor_router
 app.include_router(successor_router)
 app.include_router(master_data_router)
 app.include_router(seeding_data_router)
+app.include_router(seed_quantity_router)
