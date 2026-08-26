ed428d3115ef61e2db2e6a103a284e076380dca4
imp050t0r <106441783+imp050t0r@users.noreply.github.com>
2026-08-26 10:17:10 +0200

    Set backend version to 1.24.5

--- backend/app/server.py
+++ backend/app/server.py
@@ -18,7 +18,7 @@ from app.seeding_data_routes import router as seeding_data_router
 from app.successor_routes import router as successor_router
 
 
-APP_VERSION = "1.24.4"
+APP_VERSION = "1.24.5"
 main_module.APP_VERSION = APP_VERSION
 app = main_module.app
 app.version = APP_VERSION
