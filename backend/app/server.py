f2215b64d2eba0a35655d455aa40a8200aa1ecb7
imp050t0r <106441783+imp050t0r@users.noreply.github.com>
2026-09-01 15:20:48 +0200

    Bump GrowMaster to 1.24.20

--- backend/app/server.py
+++ backend/app/server.py
@@ -20,7 +20,7 @@ from app.seeding_data_routes import router as seeding_data_router
 from app.successor_routes import router as successor_router
 
 
-APP_VERSION = "1.24.19"
+APP_VERSION = "1.24.20"
 main_module.APP_VERSION = APP_VERSION
 app = main_module.app
 app.version = APP_VERSION
